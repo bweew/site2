ac1fb51c7f4412b84ecec6a4fcbe3861cc4dcb53
bweew <crazyflashmovies@gmail.com>
2023-08-07 01:05:58 -0400

    added csv2html

---
+++ 10/csv2html/csv2html.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from jinja2 import Template
+
+# Load CSV data using pandas
+csv_file = 'youtube_links.csv'
+df = pd.read_csv(csv_file, header=None)  # Specify header=None to indicate there are no headers
+
+# Convert DataFrame to list of dictionaries
+videos = []
+for index, row in df.iterrows():
+    videos.append({'title': row[0], 'link': row[1]})
+
+# Load the HTML template
+with open('template.html', 'r') as template_file:
+    template_content = template_file.read()
+    template = Template(template_content)
+
+# Render the HTML using the template and data
+html_output = template.render(videos=videos)
+
+# Write the HTML output to a file
+with open('output.html', 'w') as output_file:
+    output_file.write(html_output)
