2c1c27a4e3a7403e05f68839f69f27a478b46aa9
bweew <62916872+bweew@users.noreply.github.com>
2023-08-22 03:01:18 -0400

    pushed from ipad

--- 10/csv2html/csv2html.py
+++ 10/csv2html/csv2html.py
@@ -2,16 +2,20 @@ import pandas as pd
 from jinja2 import Template
 
 # Load CSV data using pandas
-csv_file = 'youtube_links.csv'
-df = pd.read_csv(csv_file, header=None)  # Specify header=None to indicate there are no headers
+csv_file = "youtube_links.csv"
+df = pd.read_csv(
+    csv_file, header=None
+)  # Specify header=None to indicate there are no headers
 
 # Convert DataFrame to list of dictionaries
 videos = []
 for index, row in df.iterrows():
-    videos.append({'title': row[0], 'link': row[1]})
+    videos.append({"title": row[0], "link": row[1]})
 
-# Load the HTML template
-with open('template.html', 'r') as template_file:
+# Load the HTML template with proper encoding
+template_file_path = "template.html"
+template_encoding = "utf-8"  # Specify the correct encoding of your template file
+with open(template_file_path, "r", encoding=template_encoding) as template_file:
     template_content = template_file.read()
     template = Template(template_content)
 
@@ -19,5 +23,7 @@ with open('template.html', 'r') as template_file:
 html_output = template.render(videos=videos)
 
 # Write the HTML output to a file
-with open('output.html', 'w') as output_file:
+output_file_path = "output.html"
+with open(output_file_path, "w", encoding=template_encoding) as output_file:
     output_file.write(html_output)
+	
