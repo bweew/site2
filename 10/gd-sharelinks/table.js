3cab235e1ef5560381ff6734711a4d7a9751f695
bweew <thon2na@gmail.com>
2024-03-03 12:12:27 -0500

    Provided Input

---
+++ 10/gd-sharelinks/table.js
@@ -0,0 +1,21 @@
+// This script will dynamically generate a table of links from a CSV file
+$(document).ready(function() {
+  // Load CSV file
+  $.get('data.csv', function(csvData) {
+    // Parse CSV data
+    var rows = csvData.split('\n');
+    var tableHTML = '<table>';
+    for (var i = 0; i < rows.length; i++) {
+      var columns = rows[i].split(',');
+      if (columns.length === 2) {
+        var url = columns[0].trim();
+        var title = columns[1].trim();
+        tableHTML += '<tr><td><a href="' + url + '">' + title + '</a></td></tr>';
+      }
+    }
+    tableHTML += '</table>';
+
+    // Append table to container
+    $('#tableContainer').html(tableHTML);
+  });
+});
